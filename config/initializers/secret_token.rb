7074daec2719937d3027b5d5da968b7d350337bc
David Whitby <whitby3001@gmail.com>
2011-09-03 12:27:53 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caliburn::Application.config.secret_token = '4858f8b6bea8424d2d123af643b42333dfaa2915566a0435b9862fff2729d381624aacec3048f70711960a09d52eb784f1c3be78d2e5d2b5370f3860d570d803'
