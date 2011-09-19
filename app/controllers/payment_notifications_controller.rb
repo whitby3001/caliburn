04fa2ed3e9d6769d97b70bf323f59874cb1372d0
David Whitby <whitby3001@gmail.com>
2011-09-19 20:47:09 +0100

    Fixes

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -3,8 +3,8 @@ class PaymentNotificationsController < ApplicationController
 
   # POST /payment_notifications
   def create
-    if status == "Completed" && params[:secret] == PAYPAL_CONFIG[:secret] &&
-          params[:receiver_email] == PAYPAL_CONFIG[:email] &&
+    if status == "Completed" && params[:secret] == PAYPAL_CONFIG['secret'] &&
+          params[:receiver_email] == PAYPAL_CONFIG['email'] &&
           params[:mc_gross] == current_basket.total_price.to_s && params[:mc_currency] == "GBP"
       PaymentNotification.create!(:params => params, :basket_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id])
       render :nothing => true
