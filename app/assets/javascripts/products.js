8fbec0691e2cf576969b9e409a4fc67578f5a3d1
David Whitby <whitby3001@gmail.com>
2011-09-29 10:41:33 +0100

    Fancybox slideshow update

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   $(".product-thumbs span a").mouseenter(function() {
+    $('.product-thumbs span div a').attr('rel', 'main-product-images');
     var newImage = $(this).parent().find("div a").clone().fancybox();
     $("#main-product-image a").replaceWith(newImage);
+    $(this).parent().find("div a").removeAttr('rel');
+    $("a[rel=main-product-images]").fancybox();
   });
   
   $("a[rel=product-images]").fancybox();
+  $("a[rel=main-product-images]").fancybox();
 });
